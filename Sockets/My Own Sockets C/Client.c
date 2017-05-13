b1da69372b40e5f38d498284509049fe59e0fa11
lucasnnobrega <lucasnnobrega@users.noreply.github.com>
2017-05-13 18:42:50 -0300

    Servidor em processo de criação com comentários

--- Sockets/My Own Sockets C/Client.c
+++ Sockets/My Own Sockets C/Client.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <sys/socket.h>
 
-int main()
-{
-    int status_connect = connect(socket_desc, &)
-/*
-sockid: integer, socket to be used in connection
-foreignAddr: struct sockaddr: address of the passive participant
-addrlen: integer, sizeof(name)
-status: 0 if successful connect, -1 otherwise
+//int main()
+//{
+/*    int status_connect = connect(socket_desc, )
+    ///sockid:           integer, socket to be used in connection
+    ///foreignAddr:      struct sockaddr: address of the passive participant
+    ///addrlen:          integer, sizeof(name)
+    if(status_connect == 0)
+        printf("O connect ocorreu com sucesso. \n");
+    else
+        printf("Erro de connect. \n");
+
+
+
+
+
 */
-
-
-}
+//}
--- Sockets/My Own Sockets C/Server.c
+++ Sockets/My Own Sockets C/Server.c
@@ -8,29 +8,71 @@ int main(int argc, char *argv[])
     int socket_desc;
 
     socket_desc = socket(AF_INET, SOCK_STREAM, 0);
-    //AF_INET         IP version 4
-    //SOCK_STREAM     oriented to TCP
-    //0               Protocol
+    ///AF_INET         IP versão 4
+    ///SOCK_STREAM     Orientado a TCP
+    ///0               Protocolo
 
-    if(socket_desc == -1){
-        printf("Could not create socket");
-
-    }
+    if(socket_desc >= 0)
+        printf("Socket criado com sucesso.\n");
+    if(socket_desc == -1)
+        printf("Não foi possível criar o socket. \n");
 
     //////////////////////////////////////////////////////////
-    ///////////////////      BIND      ///////////////////////
+    ////////////      SOCKADDR_IN      ///////////////////////
     //////////////////////////////////////////////////////////
+
 
     struct sockaddr_in server;
     server.sin_addr.s_addr = inet_addr("127.0.0.1");
     server.sin_family = AF_INET;
     server.sin_port = htons(12345);
 
-    int status_bind = bind(socket_desc, (struct sockaddr *) &server, 100);
+
+    //////////////////////////////////////////////////////////
+    ///////////////////      BIND      ///////////////////////
+    //////////////////////////////////////////////////////////
+
+    int status_bind = bind(socket_desc, (struct sockaddr *) &server, sizeof(server));
+    ///socket_desc     Descrição do Socket
+    ///addrport        Estrutura com o endereço e descrição do socket
+    ///size            Tamanho em bytes de Estrutura
+
+    if(status_bind == 0)
+        printf("O bind ocorreu com sucesso. \n");
+    else
+        printf("Erro de bind. \n");
+
+
+    //////////////////////////////////////////////////////////
+    ///////////////////      LISTEN      /////////////////////
+    //////////////////////////////////////////////////////////
 
     int status_listen = listen(socket_desc, 2);
+    ///socket_desc     Descrição do Socket
+    ///queuelen        int numero de participantes ativos esperando conectar
+    if(status_listen == 0)
+        printf("O listen ocorreu com sucesso. \n");
+    else
+        printf("Erro de listen. \n");
 
-    int s = accept(sockit, )
+    //////////////////////////////////////////////////////////
+    ///////////////////      ACCEPT     //////////////////////
+    //////////////////////////////////////////////////////////
+
+    int s = accept(socket_desc, (struct sockaddr *) &server, sizeof(server));
+    ///socket_desc     Descrição do Socket
+    ///addrport        Estrutura com o endereço e descrição do socket
+    ///size            Tamanho em bytes de Estrutura
+
+
+    if(s >= 0)
+        printf("Accept criado com sucesso.\n");
+    if(s == -1)
+        printf("Erro de Accept. \n");
+
+
+
+
 
 
     //accept();
